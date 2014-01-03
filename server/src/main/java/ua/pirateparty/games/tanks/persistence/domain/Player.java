024fa68a9a73bcb54bfc13f5913f8ecbabb84f90
Kindrat <legioner@pp-ua.org>
2014-01-03 02:38:29 +0200

    Небольшая надстройка в мейвене

--- server/src/main/java/ua/pirateparty/games/tanks/persistence/domain/Player.java
+++ server/src/main/java/ua/pirateparty/games/tanks/persistence/domain/Player.java
@@ -21,4 +21,52 @@ public class Player {
     private String nickname;
     @Column(name = "password")
     private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
